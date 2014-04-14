abe8df3342aa713e79c90cd4aee4517c71a2aad0
Gerardo Lopez-Fernandez <gerir@evernote.com>
2014-04-15 00:45:10 +0200

    version bump

--- lib/everpager/about.rb
+++ lib/everpager/about.rb
@@ -2,5 +2,5 @@ require 'everpager/version'
 
 module Everpager
   ME = :everpager
-  ABOUT = "#{ME} v#{VERSION}\nCopyright (c) 2012 by Evernote Corporation\nInternal use only."
+  ABOUT = "#{ME} v#{VERSION}\nCopyright (c) 2012 by Evernote Corporation"
 end
